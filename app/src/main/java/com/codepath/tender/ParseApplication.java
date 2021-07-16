3a65ca5c7f62f3a4fd5de5aef3457ff19035e0bd
tessvandaele <65978033+tessvandaele@users.noreply.github.com>
2021-07-16 08:45:23 -0500

    Adjusting Restaurant model for parse

---
+++ app/src/main/java/com/codepath/tender/ParseApplication.java
@@ -0,0 +1,26 @@
+package com.codepath.tender;
+
+/* connects to the backend to access and edit user data */
+
+import android.app.Application;
+
+import com.codepath.tender.models.Restaurant;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //register our parse models
+        ParseObject.registerSubclass(Restaurant.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("CnDW4tVgE6ByWBz3m83vMXHY8Lz4GDqSJ9lb4ame")
+                .clientKey("h5phc5tO6UJz2VMbM1vui3Uy5W3nwEHMStDutePq")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
--- app/src/main/java/com/codepath/tender/models/Restaurant.java
+++ app/src/main/java/com/codepath/tender/models/Restaurant.java
@@ -5,85 +5,79 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
 import org.parceler.Parcel;
 
 import java.text.DecimalFormat;
 
-@Parcel
-@Entity(tableName = "restaurant_table")
-public class Restaurant {
+@ParseClassName("Post")
+public class Restaurant extends ParseObject {
 
-    @PrimaryKey
-    @NonNull
-    private String name;
-    private String image_url;
-    private float distance;
-    private float rating;
-    private String phone;
-    private String price;
-    private int review_count;
+    public static final String NAME_KEY = "name";
+    public static final String IMAGE_KEY = "image_url";
+    public static final String DISTANCE_KEY = "distance";
+    public static final String RATING_KEY = "rating";
 
     public Restaurant() {}
 
-    public String getName(){return this.name;}
+    public String getName(){return getString(NAME_KEY);}
 
     public String getImage_url() {
-        return image_url;
+        return getString(IMAGE_KEY);
     }
 
     public float getDistance() {
-        return distance;
+        return getLong(DISTANCE_KEY);
     }
 
     public float getRating() {
-        return rating;
+        return getLong(RATING_KEY);
     }
 
     public String getPhone() {
-        return phone;
+        return "phone";
     }
 
     public String getPrice() {
-        return price;
+        return "price";
     }
 
     public int getReview_count() {
-        return review_count;
+        return 0;
     }
 
     public void setName(@NonNull String name) {
-        this.name = name;
+        put(NAME_KEY, name);
     }
 
     public void setImage_url(String image_url) {
-        this.image_url = image_url;
+        put(IMAGE_KEY, image_url);
     }
 
     public void setDistance(float distance) {
-        this.distance = distance;
+        put(DISTANCE_KEY, distance);
     }
 
     public void setRating(float rating) {
-        this.rating = rating;
+        put(RATING_KEY, rating);
     }
 
     public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public void setPrice(String price) {
-        this.price = price;
     }
 
     public void setReview_count(int rating_count) {
-        this.review_count = rating_count;
     }
 
     //helper method that returns a display string of the distance in miles
     public String getDisplayDistance() {
         float milesPerMeter = 0.000621371f;
         DecimalFormat df = new DecimalFormat("#.#");
-        float distanceInMiles = milesPerMeter * distance;
+        float distanceInMiles = milesPerMeter * getLong(DISTANCE_KEY);
         return df.format(distanceInMiles) + " mi";
 
     }
