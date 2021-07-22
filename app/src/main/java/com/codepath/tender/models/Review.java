077fae93931693b19f747566180f116a0f4eefc1
tessvandaele <65978033+tessvandaele@users.noreply.github.com>
2021-07-22 10:39:15 -0500

    Creating models and layout for review data

--- app/src/main/java/com/codepath/tender/models/Review.java
+++ app/src/main/java/com/codepath/tender/models/Review.java
@@ -3,4 +3,10 @@ package com.codepath.tender.models;
 public class Review {
 
     public Review() { }
+
+    private String text;
+    private int rating;
+    private String time_created;
+    private User user;
+
 }
---
+++ app/src/main/java/com/codepath/tender/models/User.java
@@ -0,0 +1,8 @@
+package com.codepath.tender.models;
+
+public class User {
+
+    private String name;
+    private String image_url;
+
+}
