5eb48327ec79936eaa6b6c220664278335de1edd
tessvandaele <65978033+tessvandaele@users.noreply.github.com>
2021-07-22 14:29:05 -0500

    Layout improvements on details activity

--- app/src/main/java/com/codepath/tender/adapters/ReviewAdapter.java
+++ app/src/main/java/com/codepath/tender/adapters/ReviewAdapter.java
@@ -3,6 +3,7 @@ package com.codepath.tender.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,9 +19,17 @@ import com.bumptech.glide.Glide;
 import com.codepath.tender.R;
 import com.codepath.tender.models.Review;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder>{
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
 
     private List<Review> reviews;
     private Context context;
@@ -72,14 +81,18 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder
         //binding the data to the view holder
         public void bind(Review review) {
             user_name.setText(review.getUser().getName());
-            created_at.setText(review.getTime_created());
+            created_at.setText(getRelativeTimeAgo(review.getTime_created()));
             user_rating.setRating(review.getRating());
             user_text.setText(review.getText());
 
-            Glide.with(context)
-                    .load(review.getUser().getImage_url())
-                    .circleCrop()
-                    .into(user_image);
+            if(review.getUser().getImage_url() != null) {
+                Glide.with(context)
+                        .load(review.getUser().getImage_url())
+                        .circleCrop()
+                        .into(user_image);
+            }
+
+
 
             review_link.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -89,6 +102,40 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder
                 }
             });
 
+        }
+
+        //returns relative time stamp
+        public String getRelativeTimeAgo(String rawJsonDate) {
+            String twitterFormat = "yyyy-MM-dd hh:mm:ss";
+            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+            sf.setLenient(true);
+
+            try {
+                long time = sf.parse(rawJsonDate).getTime();
+                long now = System.currentTimeMillis();
+
+                final long diff = now - time;
+                if (diff < MINUTE_MILLIS) {
+                    return "just now";
+                } else if (diff < 2 * MINUTE_MILLIS) {
+                    return "a minute ago";
+                } else if (diff < 50 * MINUTE_MILLIS) {
+                    return diff / MINUTE_MILLIS + " m";
+                } else if (diff < 90 * MINUTE_MILLIS) {
+                    return "an hour ago";
+                } else if (diff < 24 * HOUR_MILLIS) {
+                    return diff / HOUR_MILLIS + " h";
+                } else if (diff < 48 * HOUR_MILLIS) {
+                    return "yesterday";
+                } else {
+                    return diff / DAY_MILLIS + " days ago";
+                }
+            } catch (ParseException e) {
+                Log.i("TweetsAdapter", "getRelativeTimeAgo failed");
+                e.printStackTrace();
+            }
+
+            return "";
         }
     }
 }
